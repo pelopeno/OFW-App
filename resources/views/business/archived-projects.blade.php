06b0c2f3dbf1f7a590d4d772988dd42e66d1ae68
Ed Jerome Manalo <manaloedjerome@gmail.com>
2025-12-11 06:27:33 +0800

    Fix title typo in archived projects page (temporary)

--- resources/views/business/archived-projects.blade.php
+++ resources/views/business/archived-projects.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Archived Projects</title>
+    <title>Archived Project</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
